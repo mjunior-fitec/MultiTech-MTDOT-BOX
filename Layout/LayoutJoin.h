b3bccc99b3e9a1403ad000c06d7786832e71251c
Ryan Klaassen <ryan.klaassen@multitech.com>
2016-08-16 14:09:23 -0500

    Replace LayoutJoin.h

--- Layout/LayoutJoin.h
+++ Layout/LayoutJoin.h
@@ -28,13 +28,13 @@ class LayoutJoin : public Layout {
         ~LayoutJoin();
 
         void display();
-        void displayEditFsb(string rate, uint32_t power, uint8_t band, string key, string id);
+        void displayEditFsb(uint8_t rate, uint32_t power, uint8_t band, string key, string id);
 
         void updateId(std::string id);
         void updateKey(std::string key);
         void updateFsb(uint8_t band);
         void updateJoinFsb(uint8_t band);
-        void updateRate(std::string rate);
+        void updateRate(uint8_t rate);
         void updatePower(uint32_t power);
         void updateAttempt(uint32_t attempt);
         void updateStatus(std::string status);
