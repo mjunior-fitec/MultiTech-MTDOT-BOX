747c30c39e5147198841420e7c5149a61982d034
Leon Lindenfelser <llindenfelser@multitech.com>
2016-10-17 10:24:10 -0500

    1. Display DOT-BOX/EVB AU915/US915/EU868 on start up and main menu screens. 2. Add check for SF_10 in demo mode. Diplay message and change to SF_9 so packet size is large enough for all data. 3. Update setup.sh to pull in libs with enable/disable irq removed for mbed 5.

--- setup.sh
+++ setup.sh
@@ -31,12 +31,12 @@ mdot-library/version.sh
 echo "creating libs directory..."
 mkdir -p $LIB
 
-update_hg_lib "ISL29011" 3
-update_hg_lib "MMA845x" 3
-update_hg_lib "MPL3115A2" 2
+update_hg_lib "ISL29011" 4
+update_hg_lib "MMA845x" 4
+update_hg_lib "MPL3115A2" 3
 update_hg_lib "NCP5623B" 3
 update_hg_lib "GpsParser" 10
-update_hg_lib "DOGS102" 4
+update_hg_lib "DOGS102" 5
 
 echo "generating version header..."
 cd $DIR
