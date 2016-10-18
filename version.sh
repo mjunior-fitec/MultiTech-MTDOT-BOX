8c9b22c1fc1ef50705bb053cc9cc68ed0ec1dc75
Mike Fiore <mfiore@multitech.com>
2016-10-18 10:06:20 -0500

    remove branch stuff from version.sh

--- version.sh
+++ version.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-VERSION=`git describe --tags`
-BRANCH=$(git branch | grep '^*' | sed 's/^\* //')
-
-if [[ $BRANCH != "master" ]] && [[ $BRANCH != "(no branch)" ]]; then
-    VERSION="$VERSION-$BRANCH"
-fi
+VERSION=`git describe`
 
 echo -e "#ifndef __VERSION_H__\r\n#define __VERSION_H__\r\n\r\n#define MTDOT_BOX_VERSION \"$VERSION\"\r\n\r\n#endif" > dotbox_version.h
